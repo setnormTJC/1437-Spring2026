eaaf28e39446f47a856e70bf8f1921b68bac24b7
Work <Work@MSI>
2026-04-01 16:04:59 -0500

    Where we ended on April 1.
    Next time, consider demoing the priority queue or the map data structure with a CUSTOM class data type (which requires overloading < operator).

---
+++ April1-simple example of operator overloading/April1-simple example of operator overloading.cpp
@@ -0,0 +1,122 @@
+// April1-simple example of operator overloading.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include<algorithm> //spoiler alert: this is an IMPORTANT header file
+#include<iomanip>
+#include <iostream>
+#include<vector> 
+
+using namespace std; 
+
+
+class Shoe
+{
+public: 
+
+	string name; //Adidas somethings
+	int size; //size 7, 11, etc. 
+
+	float cost; //ex: 13.99
+
+
+	/*compares shoes by their COSTS (not by their sizes - this choice was ARBITRARY) */
+	bool operator < (Shoe rightHandShoe)
+	{
+		if (this->cost < rightHandShoe.cost)
+		{
+			return true; 
+		}
+
+		else
+		{
+			return false; 
+		}
+	};
+
+	bool isGreaterShoeSize(Shoe rhsShoe)
+	{
+		return this->size > rhsShoe.size; 
+	}
+	
+	void print() const
+	{
+		const int NAME_COLUMN_WIDTH = 25;
+		const int SIZE_COLUMN_WIDTH = 10;
+		const int COST_COLUMN_WIDTH = 10; 
+
+		const int TOTAL_ROW_WIDTH = NAME_COLUMN_WIDTH + SIZE_COLUMN_WIDTH + COST_COLUMN_WIDTH;
+		
+		string separator(TOTAL_ROW_WIDTH, '=');
+		
+		//cout << "=================================================" //the dumb way
+
+		cout << separator << "\n";
+
+		cout << left << setw(NAME_COLUMN_WIDTH) << name;
+		cout << left << setw(SIZE_COLUMN_WIDTH) << size;
+		cout << left << setw(COST_COLUMN_WIDTH) << cost;
+		cout << "\n";
+
+	}
+}; //end Shoe class def.
+
+
+void printShoeList(const vector<Shoe> shoeList)
+{
+
+	const int NAME_COLUMN_WIDTH = 25;
+	const int SIZE_COLUMN_WIDTH = 10;
+	const int COST_COLUMN_WIDTH = 10;
+
+	cout << left << setw(NAME_COLUMN_WIDTH) << "NAME";
+	cout << left << setw(SIZE_COLUMN_WIDTH) << "SIZE";
+	cout << left << setw(COST_COLUMN_WIDTH) << "COST (USD)";
+	cout << "\n";
+	const int TOTAL_ROW_WIDTH = NAME_COLUMN_WIDTH + SIZE_COLUMN_WIDTH + COST_COLUMN_WIDTH;
+
+
+	for (Shoe currentShoe : shoeList) //range-based for loop (no use of int i = 0; ./.blah
+	{
+		//cout << currentShoe << "\n"; //how to make this work? (remind me next time, if you like
+		currentShoe.print(); 
+	}
+}
+
+int main()
+{
+
+	Shoe myShoe = { "Altra Olympus V", 11, 139.99 }; //Altra Olympus 5
+
+	Shoe JJsShoe = { "Yeezy's", 10, 59.99 }; //Yeezys! 
+
+	Shoe theMostExpensiveAmazonShoe =
+	{
+		"Nike Men's Free Run",
+		9,
+		780.99
+	};
+
+	vector<Shoe> shoes =
+	{
+		myShoe,
+		JJsShoe,
+		theMostExpensiveAmazonShoe,
+		{"Adidas", 6, 45.99} //this is called an "anonymous object" in OOP (C++
+	};
+
+
+	//shoes.at(1).print(); 
+
+
+	//shoes[0].print(); 
+
+	cout << "\n\nBEFORE sorting the shoes:\n";
+	printShoeList(shoes); 
+
+	std::sort(shoes.begin(), shoes.end()); //sorted by COST
+
+	cout << "\n\nAFTER sorting the shoes:\n";
+	//call print
+	printShoeList(shoes); 
+
+}
